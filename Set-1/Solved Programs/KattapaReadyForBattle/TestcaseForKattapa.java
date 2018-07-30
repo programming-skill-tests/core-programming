5768d32247b744544cc99cba87544929cb0f030d
pavansmg <40863313+pavansmg@users.noreply.github.com>
2018-07-30 09:54:22 +0530

    Changes are Applied

--- Set-1/Solved Programs/KattapaReadyForBattle/TestcaseForKattapa.java
+++ Set-1/Solved Programs/KattapaReadyForBattle/TestcaseForKattapa.java
@@ -6,11 +6,21 @@ import org.junit.Test;
 
 public class TestcaseForKattapa {
 
+
 	@Test
-	public  void testKattapa() {
-		
-		assertEquals("READY FOR BATTLE",KattapaReadyForBattle.isReadForBattle(5, new int[]{9,11,12,13,14}));
-		
+	public void kattapaReadyForBattlePositiveTest_Result  () {
+	  
+	   assertEquals("READY FOR BATTLE", KattapaReadyForBattle.isReadyForBattle(5, new int[]{2, 3, 4, 5,6}));
+	   
+	   assertEquals("NOT READY", KattapaReadyForBattle.isReadyForBattle(4, new int[]{11, 13, 14, 15}));
+	}
+	
+	@Test
+	public void kattapaReadyForBattleNegativeTest_Result  () {
+	  
+		assertEquals("Number of Soliders and weapon not matching ", KattapaReadyForBattle.isReadyForBattle(2, new int[]{11, 12, 13, 14}));
+	   
+	    assertEquals("Invalid input", KattapaReadyForBattle.isReadyForBattle(4, null));
 	}
 
 }
