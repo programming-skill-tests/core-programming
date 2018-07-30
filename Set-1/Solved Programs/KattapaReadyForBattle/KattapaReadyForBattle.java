ef56c9187b69e3e56316ed889f3187101bdd3100
pavansmg <40863313+pavansmg@users.noreply.github.com>
2018-07-30 19:39:46 +0530

    Requested changes are done.

--- Set-1/Solved Programs/KattapaReadyForBattle/KattapaReadyForBattle.java
+++ Set-1/Solved Programs/KattapaReadyForBattle/KattapaReadyForBattle.java
@@ -2,40 +2,51 @@ package com.problems;
 
 import java.util.logging.Logger;
 
+
+/**
+ * The class  KattapaReadyForBattle
+ *
+ */
 public class KattapaReadyForBattle {
 
-    public static String isReadyForBattle(int n,int[] soldierWeapons){
-    	
-    	/** The logger. */
-	   Logger logger = Logger.getLogger(KattapaReadyForBattle.class.getName());
-	    
-       int evenWeapons=0,oddWeapons=0;
-       
-        for(int i=0;i < n; i++){
-        	
-            if(soldierWeapons[i]%2 == 0){
-            	
-            	evenWeapons++;
-                
-            }else{
-            	
-            	oddWeapons++;
-                
-            }
-        }
-        if(evenWeapons > oddWeapons){
-        	
-        	logger.info("READY FOR BATTLE");
-            return "READY FOR BATTLE";
-            
-        }
-        else{
-        	
-        	logger.info("NOT READY");
-            return "NOT READY";
-            
-        }
-        
-    }
-}
+	/** The logger. */
+	static Logger logger = Logger.getLogger(KattapaReadyForBattle.class.getName());
 
+	/**
+	 * Kattapa is Ready for Battle
+	 * 
+	 * @param n 
+	 * @param soldierWeapons
+	 *            
+	 * @return READY FOR BATTLE or NOT READY
+	 */
+	public static String isReadyForBattle(int n, int[] soldierWeapons) {
+
+		int evenWeapons = 0, oddWeapons = 0;
+
+		for (int i = 0; i < n; i++) {
+
+			if (soldierWeapons[i] % 2 == 0) {
+
+				evenWeapons++;
+
+			} else {
+
+				oddWeapons++;
+
+			}
+		}
+		if (evenWeapons > oddWeapons) {
+
+			logger.info("READY FOR BATTLE");
+			return "READY FOR BATTLE";
+
+		} else {
+
+			logger.info("NOT READY");
+			return "NOT READY";
+
+		}
+
+	}
+}
