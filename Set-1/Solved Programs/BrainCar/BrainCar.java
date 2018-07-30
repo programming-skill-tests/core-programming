f771f64c9f0422873fed1ce820adbaaecd037330
pavansmg <40863313+pavansmg@users.noreply.github.com>
2018-07-30 09:52:31 +0530

    Changes are Applied

--- Set-1/Solved Programs/BrainCar/BrainCar.java
+++ Set-1/Solved Programs/BrainCar/BrainCar.java
@@ -1,51 +1,74 @@
 package com.braincar;
 
+import java.util.logging.Logger;
+
 public class BrainCar {
-	public static String checkBrainCarName(String s) {
-        
-        //String s="aabbcc";
-				int count=1;
-				int diff[]=new int[3];				
-				int c=0;
-				int len=s.length();
-				if(s.charAt(0)!=s.charAt(len-1))
+	
+	private static int eachconsecutiveCount=1;
+	private static int consecutiveDifferentVar[]=new int[3];				
+	private static int consecutiveRepeatCount=0;
+	
+	
+	/**
+	 * Checking BrainCarName
+	 *
+	 * @param name of the car
+	 * @return OK or Not OK
+	 */
+	public static String checkBrainCarName(String name) {
+		
+		int len=name.length();
+				
+				
+				/** The logger. */
+			    Logger logger = Logger.getLogger(BrainCar.class.getName());
+				
+				if(name.charAt(0)!=name.charAt(len-1))
 				{
-					for(int i=1;i<s.length();i++)
+					for(int i=1;i<name.length();i++)
 					{
 						
+						getConsecutiveCharcterCount(i,name);
 						
-						if(s.charAt(i)==s.charAt(i-1))
-						{
-							count++;
-						}
-						else
-						{
-							diff[c]=count;						
-							count=1;
-							c++;
-							
-						}
-						if(c==3)
+						if(consecutiveRepeatCount==3)
 							break;
 											
 					}
 				
-					if(c==2)
+					if(consecutiveRepeatCount==2)
 					{
-						diff[c]=count;
+						consecutiveDifferentVar[consecutiveRepeatCount]=eachconsecutiveCount;
 					}
-					//System.out.println(c+" "+diff[0]+" "+diff[1]+" "+diff[2]);
+					
 				}
-				if(c==2&&diff[0]==diff[1]&&diff[1]==diff[2])
+				if(consecutiveRepeatCount==2 && consecutiveDifferentVar[0] == consecutiveDifferentVar[1] && consecutiveDifferentVar[1] == consecutiveDifferentVar[2])
 				{
-					System.out.println("OK");
+					logger.info("OK");
 					return "OK";
 				}
 				else
 				{
-					System.out.println("Not OK");
+					logger.info("Not OK");
 					return "Not OK";
 				}
+				
+			
     }
+	
+	public static  void getConsecutiveCharcterCount(int i, String name){
+		
+		if(name.charAt(i)==name.charAt(i-1))
+		{
+			eachconsecutiveCount++;
+		}
+		else
+		{
+			consecutiveDifferentVar[consecutiveRepeatCount]=eachconsecutiveCount;						
+			eachconsecutiveCount=1;
+			consecutiveRepeatCount++;
+			
+		}
+	
+	}
 }
 
